7e72d8599d210e1264faaaff8cebe5b2dabac57e
Ted <tedricchengkk@gmail.com>
2026-09-04 16:49:27 +0800

    Follower Growth tracker

--- hello.py
+++ hello.py
@@ -7,6 +7,16 @@ username = "cool creator"
 bio = "fun blogger"
 followers = 100
 
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+      
+
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
