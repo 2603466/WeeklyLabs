56636ae2df25c78f345750d3132c50c5b38c3d34
Ted <tedricchengkk@gmail.com>
2026-09-11 17:43:19 +0800

    Checking for errors

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -1,16 +1,37 @@
 print("Hello and Welcome to the Auditor")
 print("============================")
 stock_quantity = 0
+inventory = 0
+failed_entries = 0
+failed_entries_total = 0
 print("Stock quantity is :", stock_quantity)
 
 while True:
     stock_quantity = input("Enter stock quantity or 'Quit' to quit: ")
 
-    if stock_quantity == "Quit":
-        break
-    elif stock_quantity[0] == "-":
-        print("Invalid because negative number")
+    if stock_quantity.isdigit():
         stock_quantity = int(stock_quantity)
-        print("Stock Quantity is now :", stock_quantity)
+        inventory += stock_quantity
+        if stock_quantity == 0:
+            print("No Inventory Keyed")
+            failed_entries += 1
+            failed_entries_total += failed_entries
+            
+        if inventory > 500:
+            print("Inventory Exceed 500: "), inventory
+            failed_entires += 1
+            failed_entries_total += failed_entries
+
+        print("Total Inverntory is :", inventory)
     else:
-        print("Enter valid number or \"Quit\" to Quit") 
+        if stock_quantity == "Quit":
+            print("User has quit")
+            print("Total failed entries :", failed_entries_total)
+            break
+        elif stock_quantity[0] == "-":
+            print("Invalid because negative number")
+            failed_entries += 1
+            failed_entries_total += failed_entries
+        else: 
+            print("Invalid input") 
+
