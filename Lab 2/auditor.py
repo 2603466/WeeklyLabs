efdcf94f10aa9c2ff97e99e62bddcfc496cb58b7
Ted <tedricchengkk@gmail.com>
2026-09-11 17:12:42 +0800

    Inventory Checking

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -1,5 +1,16 @@
 print("Hello and Welcome to the Auditor")
 print("============================")
-Stock_Quantity = 0
-print("Stock quantity is :", Stock_Quantity)
-        
+stock_quantity = 0
+print("Stock quantity is :", stock_quantity)
+
+while True:
+    stock_quantity = input("Enter stock quantity or 'Quit' to quit: ")
+
+    if stock_quantity == "Quit":
+        break
+    elif stock_quantity[0] == "-":
+        print("Invalid because negative number")
+        stock_quantity = int(stock_quantity)
+        print("Stock Quantity is now :", stock_quantity)
+    else:
+        print("Enter valid number or \"Quit\" to Quit") 
