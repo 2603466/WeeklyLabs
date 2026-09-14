7e64a4aa8d55fa9e106d2cfbe87c6c2e61f38cee
Ted <2603466@sit.singaporetech.edu.sg>
2026-09-14 23:20:13 +0800

    continuous loop

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -1,1 +1,11 @@
 inventory = 0
+
+while True:
+    
+    quantity = input("Enter stock quantity, or \"q\" to quit: ")
+
+    if quantity == "q":
+        break
+
+    inventory += int(quantity)
+    print(inventory)    
