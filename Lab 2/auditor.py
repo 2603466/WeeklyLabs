35a4a41afeedfef4ba50cc0ac38b446bd67cf62f
Ted <2603466@sit.singaporetech.edu.sg>
2026-09-15 00:13:05 +0800

    Reporting

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -1,10 +1,13 @@
 inventory = 0 # running total
+failed_entries = 0
 
 while True:
     
     quantity = input("Enter stock quantity, or \"q\" to quit: ")
 
     if quantity == "q":
+        print("failed entires: ",failed_entries)
+        print("total inventory: ",inventory)
         break
 
     if quantity.isdigit() and int(quantity) >= 0:
@@ -12,6 +15,7 @@ while True:
         print(inventory)
 
     else:
+        failed_entries += 1
         print("Invalid Input")
 
     if int(inventory) > 500:
