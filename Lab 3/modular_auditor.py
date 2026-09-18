8d2ad6b267c69ee0c127779d2164d6d90d61b4fa
Ted <tedricchengkk@gmail.com>
2026-09-18 16:26:48 +0800

    Initialize the inventory

---
+++ Lab 3/modular_auditor.py
@@ -0,0 +1,9 @@
+inventory = 0
+
+def get_valid_input (quit, int)
+
+def process_delivery (current_total, new_value)
+
+def calculate_tax (amount)
+
+def generate_report (total_units, failed_attempts)
