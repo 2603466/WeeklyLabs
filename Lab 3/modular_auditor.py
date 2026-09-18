32bb3031c04cfefc40d84544924ae4c50a5ba2a7
Ted <tedricchengkk@gmail.com>
2026-09-18 18:23:24 +0800

    user enters a valid value

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -2,20 +2,40 @@ MAX_CAPACITY = 500
 TAX_RATE = 0.1
 
 def get_valid_input():
-    int = input("Input Stoack Quantity or \"quit\" to quit: " )
-    if int == "quit":
+    quantity = input("Input Stock Quantity or \"quit\" to quit: " ) #check if input is to quit
+    if quantity == "quit":
         return "q"
-    elif int.isdigit() and not "": 
-        print(int)    
+    
+    if quantity.isdigit() and int(quantity) > 0: #check for if digit is bool and if its a -ve int
+        print("valid input")
+        return int(quantity)
+
+    else:
+        print("Error not valid input")
+        return "quantity"
+
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+def calculate_tax(amount):
+    return amount * TAX_RATE
 
 def main():
     inventory = 0
-
+    
     while True:
-        if get_valid_input() == "q":
-            break
-        
+        stock = get_valid_input()
+        total_tax = calculate_tax(inventory)
 
+        if stock == "q":
+            break
+
+        else:
+            inventory = process_delivery(inventory, stock)
+            print ("Total Inventory is: ", inventory)
+
+            calculate_tax(inventory)
+            print("Total Tax is: ", total_tax)
 
 main()
 
