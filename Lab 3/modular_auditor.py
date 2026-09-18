121a459af1e2a0dc60ef1f2d26bf33d3d2ff61b4
Ted <tedricchengkk@gmail.com>
2026-09-18 18:48:33 +0800

    generate_report

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -7,12 +7,11 @@ def get_valid_input():
         return "q"
     
     if quantity.isdigit() and int(quantity) > 0: #check for if digit is bool and if its a -ve int
-        print("valid input")
         return int(quantity)
 
     else:
         print("Error not valid input")
-        return "quantity"
+        return "error"
 
 def process_delivery(current_total, new_value):
     return current_total + new_value
@@ -20,15 +19,25 @@ def process_delivery(current_total, new_value):
 def calculate_tax(amount):
     return amount * TAX_RATE
 
+def generate_report(total_units, failed_entries):
+    print("Total errors: ",failed_entries)
+    print("Total Inventory: ",total_units)
+    return
+
 def main():
     inventory = 0
+    failed_entries = 0
     
     while True:
         stock = get_valid_input()
         total_tax = calculate_tax(inventory)
 
         if stock == "q":
+            generate_report(inventory, failed_entries)
             break
+
+        elif stock == "error":
+            failed_entries += 1
 
         else:
             inventory = process_delivery(inventory, stock)
