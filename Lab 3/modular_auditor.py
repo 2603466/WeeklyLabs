99cef11074b9d854fdf26b638a932eb6255d66f2
Ted <tedricchengkk@gmail.com>
2026-09-18 17:34:53 +0800

    Run in a continuous loop

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -1,9 +1,24 @@
-inventory = 0
+MAX_CAPACITY = 500
+TAX_RATE = 0.1
 
-def get_valid_input (quit, int)
+def get_valid_input():
+    int = input("Input Stoack Quantity or \"quit\" to quit: " )
+    if int == "quit":
+        return "q"
+    elif int.isdigit() and not "": 
+        print(int)    
 
-def process_delivery (current_total, new_value)
+def main():
+    inventory = 0
 
-def calculate_tax (amount)
+    while True:
+        if get_valid_input() == "q":
+            break
+        
 
-def generate_report (total_units, failed_attempts)
+
+main()
+
+    
+
+    
